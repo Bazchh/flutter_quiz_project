68b9708658660aa41c5eaddd446e2a2def30795f
Bazch <mikael.vidal@gmail.com>
2023-12-21 12:13:35 -0300

    Classe usada para mudar o score

--- lib/MudaScore.dart
+++ lib/MudaScore.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class MudaScore extends StatelessWidget {
-  const MudaScore({super.key});
+  final int score;
+
+  const MudaScore({
+    required this.score,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Container(
+        width: 60,
+        height: 70,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50.0),
+            color: Colors.blueAccent),
+        child: Text(
+          score.toString(),
+          style: const TextStyle(
+            fontSize: 50,
+            color: Colors.black,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }

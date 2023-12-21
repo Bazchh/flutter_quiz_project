7dc0ec0860597fe7a8830247e142f19a2d12773e
Bazch <mikael.vidal@gmail.com>
2023-12-21 09:55:16 -0300

    Classe MudaScore

---
+++ lib/MudaScore.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MudaScore extends StatelessWidget {
+  const MudaScore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

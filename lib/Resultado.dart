d62d8ad81b80009420a19dd3c64c5cccdffe9529
Bazch <mikael.vidal@gmail.com>
2023-12-24 09:04:46 -0300

    Botão para reiniciar o questionario

--- lib/Resultado.dart
+++ lib/Resultado.dart
@@ -2,38 +2,51 @@ import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
   final int pontuacao;
-  Resultado(this.pontuacao);
+  final void Function() reiniciarQuestionario;
+  Resultado(this.pontuacao, this.reiniciarQuestionario);
 
-  String get fraseResultado{
-     if(pontuacao < 8){
+  String get fraseResultado {
+    if (pontuacao < 8) {
       return 'Parabéns';
-     } else if (pontuacao < 12){
+    } else if (pontuacao < 12) {
       return 'Você é bom';
-     } else {
+    } else {
       return 'Muito bom';
-     }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 290,
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.blueAccent,
-          borderRadius: BorderRadius.circular(50.0),
-        ),
-        child: Center(
-          child: Text(
-            fraseResultado,
-            style: const TextStyle(
-              fontSize: 50,
-              color: Colors.black,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            width: 290,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Center(
+              child: Text(
+                fraseResultado,
+                style: const TextStyle(
+                  fontSize: 50,
+                  color: Colors.black,
+                ),
+              ),
             ),
           ),
         ),
-      ),
+        ElevatedButton(
+          child: Text('Reiniciar?'),
+          onPressed: reiniciarQuestionario,
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent,
+              foregroundColor: Colors.white),
+        ),
+      ],
     );
   }
 }
